292b4f6cb4fa88e3b1f72a46a7a8bb1524121c3a
Mauricio <gomes98@gmail.com>
2023-01-05 16:51:50 -0300

    final 27

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -11,75 +11,107 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white.withAlpha(230),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Image.asset(item.imgUrl),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(32),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 20,
-                    spreadRadius: 10,
-                  ),
-                ],
+          // conteudo
+          Column(
+            children: [
+              Expanded(
+                // faz com que a imagem tenha uma animação de transição
+                child: Hero(
+                  tag: item.imgUrl,
+                  child: Image.asset(item.imgUrl),
+                ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          item.itemName,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                              fontSize: 27, fontWeight: FontWeight.bold),
-                        ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(32),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(50)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 20,
+                        spreadRadius: 10,
                       ),
-                      Container(
-                        height: 30,
-                        width: 70,
-                        color: Colors.red,
-                      )
                     ],
                   ),
-                  // preco
-                  Text(
-                    utilsServices.priceToCurrency(item.price),
-                    style: TextStyle(
-                      color: CustomColors.customSwatchColor,
-                      fontSize: 23,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  // Desscrição
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: SingleChildScrollView(
-                        child: Text(
-                          item.description * 20,
-                          style: const TextStyle(
-                            // fontSize: 16,
-                            // color: Colors.grey,
-                            height: 1.5,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              item.itemName,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                  fontSize: 27, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Container(
+                            height: 30,
+                            width: 70,
+                            color: Colors.red,
+                          )
+                        ],
+                      ),
+                      // preco
+                      Text(
+                        utilsServices.priceToCurrency(item.price),
+                        style: TextStyle(
+                          color: CustomColors.customSwatchColor,
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      // Desscrição
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: SingleChildScrollView(
+                            child: Text(
+                              item.description * 20,
+                              style: const TextStyle(
+                                // fontSize: 16,
+                                // color: Colors.grey,
+                                height: 1.5,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  //
+                      //
 
-                  ElevatedButton(onPressed: (() {}), child: const Text('Botão'))
-                ],
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton.icon(
+                          onPressed: (() {}),
+                          label: const Text('Add no Carrinho'),
+                          icon: const Icon(Icons.shopping_cart_outlined),
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          )),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // botão de voltar
+          Positioned(
+            left: 10,
+            top: 10,
+            child: SafeArea(
+              child: IconButton(
+                onPressed: () => Navigator.of(context).pop(),
+                icon: const Icon(Icons.arrow_back_ios),
               ),
             ),
           ),
