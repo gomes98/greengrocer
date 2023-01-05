993117d6949d3561c5e7e100be3f4226a84311c3
Mauricio <gomes98@gmail.com>
2023-01-05 16:39:57 -0300

    final da 26

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
   final ItemModel item;
-  const ProductScreen({Key? key, required this.item}) : super(key: key);
-
+  ProductScreen({Key? key, required this.item}) : super(key: key);
+  final UtilsServices utilsServices = UtilsServices();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +18,7 @@ class ProductScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: const EdgeInsets.all(32),
               decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
@@ -25,6 +28,57 @@ class ProductScreen extends StatelessWidget {
                     blurRadius: 20,
                     spreadRadius: 10,
                   ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.itemName,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                              fontSize: 27, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        height: 30,
+                        width: 70,
+                        color: Colors.red,
+                      )
+                    ],
+                  ),
+                  // preco
+                  Text(
+                    utilsServices.priceToCurrency(item.price),
+                    style: TextStyle(
+                      color: CustomColors.customSwatchColor,
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  // Desscrição
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: SingleChildScrollView(
+                        child: Text(
+                          item.description * 20,
+                          style: const TextStyle(
+                            // fontSize: 16,
+                            // color: Colors.grey,
+                            height: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  //
+
+                  ElevatedButton(onPressed: (() {}), child: const Text('Botão'))
                 ],
               ),
             ),
