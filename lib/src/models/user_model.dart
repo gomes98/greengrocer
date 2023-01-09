b3715a6bd751c2c44423e2fe1ea48a5782fafebd
Mauricio <gomes98@gmail.com>
2023-01-09 15:36:04 -0300

    final 79 UserModel

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,15 +1,50 @@
+import 'dart:ffi';
+
 class UserModel {
-  String name;
-  String email;
-  String phone;
-  String cpf;
-  String password;
+  String? name;
+  String? email;
+  String? phone;
+  String? cpf;
+  String? password;
+  String? id;
+  String? token;
 
   UserModel({
-    required this.name,
-    required this.email,
-    required this.phone,
-    required this.cpf,
-    required this.password,
+    this.name,
+    this.email,
+    this.phone,
+    this.cpf,
+    this.password,
+    this.id,
+    this.token,
   });
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      cpf: map['cpf'],
+      id: map['id'],
+      name: map['fullname'],
+      email: map['email'],
+      phone: map['phone'],
+      token: map['token'],
+      password: map['password'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "cpf": cpf,
+      "id": id,
+      "fullname": name,
+      "email": email,
+      "phone": phone,
+      "token": token,
+      "password": password,
+    };
+  }
+
+  @override
+  String toString() {
+    return "$name | $cpf";
+  }
 }
--- lib/src/pages/auth/repository/auth_repository.dart
+++ lib/src/pages/auth/repository/auth_repository.dart
@@ -1,4 +1,5 @@
 import 'package:greengrocer/src/constants/endpoints.dart';
+import 'package:greengrocer/src/models/user_model.dart';
 import 'package:greengrocer/src/services/http_manager.dart';
 
 class AuthRepository {
@@ -12,6 +13,8 @@ class AuthRepository {
 
     if (result['result'] != null) {
       print("Sigin Funcionou");
+      final user = UserModel.fromMap(result['result']);
+      print(user);
     } else {
       print("Sigin não Funcionou");
     }
