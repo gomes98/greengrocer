2925c854dab2236062a60aef93b958a04d25ec54
Mauricio <gomes98@gmail.com>
2023-01-11 10:40:16 -0300

    final 111 carrinho buscando dados

---
+++ lib/src/pages/cart/repository/cart_repository.dart
@@ -0,0 +1,25 @@
+import 'package:greengrocer/src/constants/endpoints.dart';
+import 'package:greengrocer/src/pages/cart/cart_result/cart_result.dart';
+import 'package:greengrocer/src/services/http_manager.dart';
+
+class CartRepository {
+  final _httpManager = HttpManager();
+
+  Future getCartItems({
+    required String token,
+    required String userId,
+  }) async {
+    final result = await _httpManager.restRequest(
+      url: EndPoints.getCartItems,
+      method: HttpMethods.post,
+      headers: {'X-Parse-Session-Token': token},
+      body: {'user': userId},
+    );
+
+    if (result['result'] != null) {
+      print(result['result']);
+    } else {
+      print("ocorreu um erro");
+    }
+  }
+}
